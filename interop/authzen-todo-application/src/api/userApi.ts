064d07ef156a90317b47bcd364bf16f71e925063
Omri Gazitt <ogazitt@gmail.com>
2025-02-05 19:58:32 -0800

    removed console.log

--- interop/authzen-todo-application/src/api/userApi.ts
+++ interop/authzen-todo-application/src/api/userApi.ts
@@ -4,7 +4,6 @@ const BASE_URL = import.meta.env.VITE_API_ORIGIN;
 
 export const createUserApi = (url: string, headers: Headers) => ({
   getUser: async (userId: string) => {
-    console.log(url);
     const response = await fetch(`${url ?? BASE_URL}/users/${userId}`, {
       headers: headers,
     });
