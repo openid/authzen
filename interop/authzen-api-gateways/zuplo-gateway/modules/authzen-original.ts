204931c6497c542af0ef50d790ad9523da0e5c3c
Omri Gazitt <ogazitt@gmail.com>
2025-03-05 13:04:33 -1000

    updated zuplo implementation to use native authzen policy

---
+++ interop/authzen-api-gateways/zuplo-gateway/modules/authzen-original.ts
@@ -0,0 +1,86 @@
+import { HttpProblems, ZuploContext, ZuploRequest, environment } from "@zuplo/runtime"
+
+const pdps = {
+  "Aserto": "https://authzen-gateway-proxy.demo.aserto.com",
+  "Axiomatics": "https://pdp.alfa.guide",
+  "Cerbos": "https://authzen-proxy-demo.cerbos.dev",
+  "HexaOPA": "https://interop.authzen.hexaorchestration.org",
+  "PlainID": "https://authzeninteropt.se-plainid.com",
+  "Rock Solid Knowledge": "https://authzen.identityserver.com",
+  "SGNL": "https://authzen.sgnlapis.cloud",
+  "Topaz": "https://authzen-topaz.demo.aserto.com"
+}
+const { AUTHZEN_PDP_API_KEYS } = environment
+const apiKeys = (AUTHZEN_PDP_API_KEYS && JSON.parse(AUTHZEN_PDP_API_KEYS)) ?? {}
+
+export default async function policy(
+  request: ZuploRequest,
+  context: ZuploContext,
+) {
+
+  if (!request.user) {
+    context.log.error(
+      "User is not authenticated. An authentication policy must come before the authorization policy.",
+    )
+    return HttpProblems.unauthorized(request, context)
+  }
+
+  const authzenRequest = JSON.stringify({
+    "subject": {
+      "type": "identity",
+      "id": request.user.sub
+    },
+    "resource": {
+      "type": "route",
+      "id": context.route.path
+    },
+    "action": {
+      "name": request.method,
+    },
+  })
+
+  const gatewayPdp = request.headers.get("X_AUTHZEN_GATEWAY_PDP")
+  if (!gatewayPdp) {
+    context.log.error("GATEWAY PDP URL is missing in the request headers.")
+    return HttpProblems.forbidden(request, context)
+  }
+  const pdpUrl = pdps[gatewayPdp]
+  if (!pdpUrl) {
+    context.log.error("PDP is not in certified PDP list.")
+  }
+  const gatewayPdpUrl = `${pdpUrl}/access/v1/evaluation`
+
+  try {
+    context.log.info(`Sending request to ${gatewayPdp} at ${gatewayPdpUrl}`)
+    context.log.debug(`AuthZEN request: ${authzenRequest}`)
+
+    const apiKey = apiKeys[gatewayPdp]
+    const headers = {
+      "content-type": "application/json",
+    }
+    if (apiKey) {
+      headers["Authorization"] = apiKey
+    }
+
+    const authzenResponse = await fetch(gatewayPdpUrl, { 
+      headers,
+      method: 'POST',
+      body: authzenRequest
+    })
+    const response = await authzenResponse.json()
+    context.log.debug(`AuthZEN response: ${JSON.stringify(response)}`)
+
+    if (response && response.decision) {
+      return request
+    }
+    context.log.error(
+      `The user '${request.user.sub}' is not authorized to perform this action.`,
+    )
+    return HttpProblems.forbidden(request, context);
+  } catch (e) {
+    context.log.error(
+      `AuthZEN authorization error. The user '${request.user.sub}' is not authorized to perform this action.`,
+    )
+    return HttpProblems.forbidden(request, context)
+  }
+}
--- interop/authzen-api-gateways/zuplo-gateway/modules/authzen.ts
+++ interop/authzen-api-gateways/zuplo-gateway/modules/authzen.ts
@@ -1,4 +1,4 @@
-import { HttpProblems, ZuploContext, ZuploRequest, environment } from "@zuplo/runtime"
+import {ZuploContext, ZuploRequest, AuthZenInboundPolicy, HttpProblems, environment} from "@zuplo/runtime";
 
 const pdps = {
   "Aserto": "https://authzen-gateway-proxy.demo.aserto.com",
@@ -16,29 +16,9 @@ const apiKeys = (AUTHZEN_PDP_API_KEYS && JSON.parse(AUTHZEN_PDP_API_KEYS)) ?? {}
 export default async function policy(
   request: ZuploRequest,
   context: ZuploContext,
+  options: never,
+  policyName: string
 ) {
-
-  if (!request.user) {
-    context.log.error(
-      "User is not authenticated. An authentication policy must come before the authorization policy.",
-    )
-    return HttpProblems.unauthorized(request, context)
-  }
-
-  const authzenRequest = JSON.stringify({
-    "subject": {
-      "type": "identity",
-      "id": request.user.sub
-    },
-    "resource": {
-      "type": "route",
-      "id": context.route.path
-    },
-    "action": {
-      "name": request.method,
-    },
-  })
-
   const gatewayPdp = request.headers.get("X_AUTHZEN_GATEWAY_PDP")
   if (!gatewayPdp) {
     context.log.error("GATEWAY PDP URL is missing in the request headers.")
@@ -48,39 +28,24 @@ export default async function policy(
   if (!pdpUrl) {
     context.log.error("PDP is not in certified PDP list.")
   }
-  const gatewayPdpUrl = `${pdpUrl}/access/v1/evaluation`
 
-  try {
-    context.log.info(`Sending request to ${gatewayPdp} at ${gatewayPdpUrl}`)
-    context.log.debug(`AuthZEN request: ${authzenRequest}`)
+  const apiKey = apiKeys[gatewayPdp]
 
-    const apiKey = apiKeys[gatewayPdp]
-    const headers = {
-      "content-type": "application/json",
-    }
-    if (apiKey) {
-      headers["Authorization"] = apiKey
-    }
+  let policy = new AuthZenInboundPolicy({
+    authorizerHostname: pdpUrl,
+    authorizerAuthorizationHeader: apiKey,
+    subject: {
+      type: "identity",
+      id: request.user.sub
+    },
+    resource: {
+      type: "route",
+      id: context.route.path,
+    },
+    action: {
+      name: request.method,
+    },
+  }, policyName)
 
-    const authzenResponse = await fetch(gatewayPdpUrl, { 
-      headers,
-      method: 'POST',
-      body: authzenRequest
-    })
-    const response = await authzenResponse.json()
-    context.log.debug(`AuthZEN response: ${JSON.stringify(response)}`)
-
-    if (response && response.decision) {
-      return request
-    }
-    context.log.error(
-      `The user '${request.user.sub}' is not authorized to perform this action.`,
-    )
-    return HttpProblems.forbidden(request, context);
-  } catch (e) {
-    context.log.error(
-      `AuthZEN authorization error. The user '${request.user.sub}' is not authorized to perform this action.`,
-    )
-    return HttpProblems.forbidden(request, context)
-  }
+  return policy.handler(request, context);
 }
