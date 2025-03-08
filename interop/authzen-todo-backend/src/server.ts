87ada9ea509678702996ee9e5f5e5b913258d53e
Andres Aguiar <andres.aguiar@okta.com>
2025-03-08 19:01:41 -0300

    chore: small fixes based on feedback

--- interop/authzen-todo-backend/src/server.ts
+++ interop/authzen-todo-backend/src/server.ts
@@ -33,6 +33,10 @@ export class Server {
     res.json(config);
   }
 
+  async listGatewayPdps(_: Request, res: Response) {
+    res.json(pdps.gatewayPdps);
+  }
+  
   async getUser(req: JWTRequest, res: Response) {
     const { userID } = req.params;
     if(req.auth.sub === userID) {
